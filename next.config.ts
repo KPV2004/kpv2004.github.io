800ac84f32e96f3d92767c983fd2fac711a3345a
KPV2004 <pannetvanicharkorl@gmail.com>
2025-03-25 16:51:30 +0700

    fix:deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Enables `next export`
+  basePath: '/my-next-app', // Change to your repo name
+  assetPrefix: '/my-next-app/', // Important for GitHub Pages
 };
 
 export default nextConfig;
